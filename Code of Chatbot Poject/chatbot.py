df8b3f570f511668bdfe75b634a847ff5c762f7d
Xihang Zhou <47622010+xihangzhou@users.noreply.github.com>
2020-03-18 20:39:32 +0800

    Pharmacy Recommendatio

--- Code of Chatbot Poject/chatbot.py
+++ Code of Chatbot Poject/chatbot.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 import os
 import sys
 import redis
+import requests
+import random
 
 from argparse import ArgumentParser
 
@@ -15,7 +17,7 @@ from linebot.exceptions import (
 )
 
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageMessage, VideoMessage, FileMessage, StickerMessage, StickerSendMessage
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage, VideoMessage, FileMessage, StickerMessage, StickerSendMessage,TemplateSendMessage, ButtonsTemplate, URITemplateAction
 )
 from linebot.utils import PY3
 
@@ -76,12 +78,81 @@ def callback():
     return 'OK'
 
 # Handler function for Text Message
-def handle_TextMessage(event):
-    print(event.message.text)
-    msg = 'You said: "' + event.message.text + '" '
+def handle_TextMessage(event): 
+
+    GOOGLE_API_KEY = 'AIzaSyAH5qFJ9JfgCdblC-6Y282wFMxCXA6TeHM'
+
+    address = event.message.text
+    addurl = 'https://maps.googleapis.com/maps/api/geocode/json?key={}&address={}&sensor=false'.format(GOOGLE_API_KEY,address)
+
+    #get current location
+    addressReq = requests.get(addurl)
+    addressDoc = addressReq.json()
+    lat = addressDoc['results'][0]['geometry']['location']['lat']
+    lng = addressDoc['results'][0]['geometry']['location']['lng']
+    
+    #get pharmacies around this location
+    pharmacySearch = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&rankby=distance&type=pharmacy&language=en-US'.format(GOOGLE_API_KEY,lat,lng)
+    pharmacyReq = requests.get(pharmacySearch)
+    nearby_pharmacy_dict = pharmacyReq.json()
+
+    #put the searching result into top20_pharmacy as a list
+    top20_pharmacy = nearby_pharmacy_dict['results']
+    res_num = (len(top20_pharmacy))
+
+    #get the pharmacy whose rating is beyond 3 
+    bravo=[]
+    for i in range(res_num):
+        try:
+            if top20_pharmacy[i]['rating'] >= 3.9:
+                print('rating',top20_pharmacy[i]['rating'])
+                bravo.append(i)
+        except:
+            KeyError
+    # if all pharmacies' rating are lower than 3, randomly choose one
+    if len(bravo) < 0:
+        content = 'there is no pharmacies around'
+        pharmacy = random.choice(top20_pharmacy) 
+    #or choose from bravo list
+    else:
+        pharmacy = top20_pharmacy[random.choice(bravo)] 
+        
+    #check the photo of this pharmacy
+    if pharmacy.get('photos') is None:
+        thumbnail_image_url = None
+    else:
+    #if has photos, choose one
+        photo_reference = pharmacy['photos'][0]['photo_reference']
+        photo_width = pharmacy['photos'][0]['width']
+        thumbnail_image_url = 'https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth={}'.format(GOOGLE_API_KEY,photo_reference,photo_width)
+    
+
+    rating = 'no rating' if pharmacy.get('rating') is None else pharmacy['rating']
+    address = 'No info' if pharmacy.get('vicinity') is None else pharmacy['vicinity']
+    details = pharmacy['name']
+    #the url of current map
+    map_url = "http://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(lat=pharmacy['geometry']['location']['lat'],long=pharmacy["geometry"]['location']['lng'],place_id=pharmacy['place_id'])
+    print(pharmacy['name'])
+
+    #reply template
+    buttons_template = TemplateSendMessage(
+        alt_text = pharmacy['name'],
+        template = ButtonsTemplate(
+            thumbnail_image_url = thumbnail_image_url,
+            title = 'Recommended for you',
+            text = details,
+            actions = [
+                URITemplateAction(
+                    label='Go',
+                    uri = map_url,
+                ),
+            ]
+        ) 
+    )
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(msg)
+        buttons_template
     )
 
 # Handler function for Sticker Message
---
+++ milestone2_screenshots and revision/Revision.md
@@ -0,0 +1,9 @@
+# Revision
+## Current Function
+* a good greeting to tell users what this bot can do now
+* when user type in a address, the bot will randomly recommend a pharmacy which is nearby this location and rated over 3.9 scores by Google Map.
+* user can click this recommended button and go to Google Map page in LINE or choose to move to Google Map app as they wish to get more details about this parmacy
+## What we will work on next
+* complete a simple news recommendation function to user to read news they like 
+* give more possibilities to the conversation between bot and users with Redis
+
